fd1fd1bbc8ebd6c44208aa607bf7f97a8e113856
Wouter van Rij <wvanrij@extenseconsulting.com>
2018-09-20 10:53:52 +0200

    Modified content to excluded required version.

--- Download-XrmPowershell.ps1
+++ Download-XrmPowershell.ps1
@@ -1,5 +1,5 @@
 ﻿Install-PackageProvider nuget -force
 Write-Output "Installing Microsoft.Xrm.Data.Powershell"
-Import-Module Microsoft.Xrm.Data.Powershell -Verbose -RequiredVersion 2.8.0
+Import-Module Microsoft.Xrm.Data.Powershell -Verbose
 Write-Output "Installing Microsoft.Xrm.Data.PowerShell"
-Install-Module Microsoft.Xrm.Data.PowerShell -Scope CurrentUser -Force -RequiredVersion 2.8.0
+Install-Module Microsoft.Xrm.Data.PowerShell -Scope CurrentUser -Force
